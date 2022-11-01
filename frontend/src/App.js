7986b288aa5897d29415842d9d93bcf7d94722e1
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-01 20:16:01 +0530

    update app.js | added login & signup route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import Signup from "./components/main/signup";
+import Login from "./components/main/login";
 import Home from "./components/main/home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
