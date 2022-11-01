c47acaa417d3535e9c81b3bdbae1b7c84457fcf9
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-01 09:53:30 +0530

    added transaction

--- Backend/route/user.js
+++ Backend/route/user.js
@@ -1,7 +1,11 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const bcrypt = require("bcrypt");
 const router = express.Router();
+
 const { User, validate } = require("../model/user");
+const auth = require("../middleware/auth");
+const { List } = require("../model/list");
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -24,6 +28,50 @@ router.post("/", async (req, res) => {
     .header("auth-token", token)
     .header("access-control-expose-headers", "auth-token")
     .send(user.username);
+});
+
+// Updating user's username & list username via mongodb transaction.
+
+router.put("/", auth, async (req, res) => {
+  const username = req.user.username;
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const session = await mongoose.startSession();
+  mongoose.connection.transaction(async (session) => {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const password = await bcrypt.hash(req.body.password, salt);
+
+      const user = await User.findOneAndUpdate(
+        { username },
+        {
+          username: req.body.username,
+          password,
+        },
+        { new: true }
+      );
+
+      await List.findOneAndUpdate(
+        { username },
+        {
+          username: req.body.username,
+        }
+      );
+
+      const token = user.generateAuthToken();
+      res
+        .header("auth-token", token)
+        .header("access-control-expose-headers", "auth-token")
+        .send(user.username);
+    } catch (err) {
+      console.log(err);
+      session.abortTransaction();
+      return res.status(400).send("Something failed");
+    } finally {
+      session.endSession();
+    }
+  });
 });
 
 module.exports = router;
